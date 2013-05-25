a0950967321647f82307671984bbf3dbd56816b8
Yvette Tsai <ytsai@bu.edu>
2013-05-25 11:53:00 -0400

    Create ImageUploadBean.java

---
+++ ImageUploadBean.java
@@ -0,0 +1,46 @@
+package edu.bu.cs.cs460.photoshare;
+
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * A bean that handles file uploads
+ *
+ * Original author G. Zervas <cs460tf@bu.edu>
+ * Modified by Yvette Tsai (ytsai@bu.edu)
+ */
+public class ImageUploadBean {
+  public Picture upload(HttpServletRequest request) throws FileUploadException {
+    RequestContext requestContext = new ServletRequestContext(request);
+    if (FileUpload.isMultipartContent(requestContext)) {
+      Picture picture = new Picture();
+      FileItemFactory fileItemFactory = new DiskFileItemFactory();
+      ServletFileUpload servletFileUpload = new ServletFileUpload(fileItemFactory);
+      List<FileItem> fileItems = servletFileUpload.parseRequest(request);
+
+      for (FileItem fileItem : fileItems) {
+        if (fileItem.isFormField()) {
+          if (fileItem.getFieldName().equals("caption")) {
+            picture.setCaption(fileItem.getString());
+          }
+        } else {
+          if (fileItem.getFieldName().equals("filename")) {
+            picture.setContentType(fileItem.getContentType());
+            picture.setSize(fileItem.getSize());
+            picture.setData(fileItem.get());
+          }
+        }
+      }
+
+      picture.createThumbnail();
+      return picture;
+    } else {
+      return null;
+    }
+  }
+}
